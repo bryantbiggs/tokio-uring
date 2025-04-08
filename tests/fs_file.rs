e21584e7221b562025c6afc29dcf01b8dee240fa
Bryant Biggs <bryantbiggs@gmail.com>
2025-04-08 16:49:41 -0500

    fix: Make clippy happy

--- tests/fs_file.rs
+++ tests/fs_file.rs
@@ -149,7 +149,7 @@ fn explicit_close() {
 fn drop_open() {
     tokio_uring::start(async {
         let tempfile = tempfile();
-        let _ = File::create(tempfile.path());
+        _ = File::create(tempfile.path());
 
         // Do something else
         let file = File::create(tempfile.path()).await.unwrap();
