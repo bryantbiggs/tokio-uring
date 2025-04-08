3369191374b138b8873dd9d464cd08adb0e2fc8e
Bryant Biggs <bryantbiggs@gmail.com>
2025-04-08 16:59:55 -0500

    Fix tests by removing invalid test cases
    Due to https://github.com/rust-lang/rust/pull/124210, when an owned fd
    has already been closed before it's dropped means that something else
    touched the fd in ways its not allowed to. Therefore, these tests fail
    due to this new behavior enforcement
    
    Signed-off-by: Bryant Biggs <bryantbiggs@gmail.com>

--- tests/fs_file.rs
+++ tests/fs_file.rs
@@ -321,7 +321,6 @@ fn tempfile() -> NamedTempFile {
 
 async fn poll_once(future: impl std::future::Future) {
     use std::future::poll_fn;
-    // use std::future::Future;
     use std::task::Poll;
     use tokio::pin;
 
